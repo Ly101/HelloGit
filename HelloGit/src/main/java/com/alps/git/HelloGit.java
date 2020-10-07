ab517c1c69e1fe9240e40036de2a3caeb9b8ca3f
alps_glb <1349467950_glb@qq.com>
2020-10-07 14:44:44 +0800

    修改文件

--- HelloGit/src/main/java/com/alps/git/HelloGit.java
+++ HelloGit/src/main/java/com/alps/git/HelloGit.java
@@ -7,5 +7,6 @@ package com.alps.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("second modify");
     }
 }
