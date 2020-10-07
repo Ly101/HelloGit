cc9dbd94ee6b895c6c8180396c6977d441520bdd
alps_glb <1349467950_glb@qq.com>
2020-10-07 15:06:53 +0800

    添加第二个HelloGit类

---
+++ HelloGit/src/main/java/com/alps/git/SecondHelloGit.java
@@ -0,0 +1,22 @@
+package com.alps.git;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Ly
+ * @date 2020/10/7 15:03
+ */
+public class SecondHelloGit {
+
+    private LocalDateTime date;
+
+    public LocalDateTime getDate()
+    {
+        return this.date;
+    }
+
+    public void setDate(LocalDateTime date)
+    {
+        this.date = date;
+    }
+}
